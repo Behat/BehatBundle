6e55db942fc9cc1e64a60c8d05c4cf5c124cf29c
everzet <ever.zet@gmail.com>
2011-07-12 23:00:07 +0300

    fixed bundle to use exceptions postfix

--- Console/Processor/InitProcessor.php
+++ Console/Processor/InitProcessor.php
@@ -115,7 +115,7 @@ use Behat\BehatBundle\Context\BehatContext,
     Behat\BehatBundle\Context\MinkContext;
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Exception\Pending;
+    Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
